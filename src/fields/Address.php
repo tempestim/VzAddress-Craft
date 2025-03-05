0c893769293a691f4850822963dc19aaa694eaf4
Ryan Nowak <ryan.nowak@tempest.im>
2025-03-05 14:37:58 -0700

    Update Address.php
    In case default address isn't setup in import configs.

--- src/fields/Address.php
+++ src/fields/Address.php
@@ -133,7 +133,7 @@ class Address extends Field
         if (isset($value['__model__'])) {
             unset($value['__model__']);
         }
-        if (isset($value) && array_key_exists('countryName', $value)) {
+        if (isset($value) && is_array($value) && array_key_exists('countryName', $value)) {
             unset($value['countryName']);
         }
 
